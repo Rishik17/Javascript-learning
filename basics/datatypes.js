6c50fc866205a833ec3120b0035f1b5dbf36cf87
Vajhala Rishik Kumar <100462258+Rishik17@users.noreply.github.com>
2024-08-01 14:47:58 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,20 @@
+"user strict"; // treat all JS code as newer verison
+
+// ; is mostly not used in JS codes
+
+let name ="Rishik"
+let age =21
+let TrueorFalse = false
+let state;
+
+//numebr ==> 2 to pow 53
+//bigint
+//String ==> ""
+//boolean ==>true/false
+//null ==> standalone value
+//undefined ==>
+//symbol =>unique
+
+
+console.log(typeof undefined);
+console.log(typeof null);  //typeof null ==> object
