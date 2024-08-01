6595b71cdedf89cd0fae604d055d5f49a4ac5f3a
Vajhala Rishik Kumar <100462258+Rishik17@users.noreply.github.com>
2024-08-01 14:31:33 +0000

    container config done

---
+++ basics/variables.js
@@ -0,0 +1,14 @@
+const accId = 12345
+let accEmail = "rishi@gmail.com"
+var accpwd = "98765"
+acccity = "Vja"
+let accstate;
+
+//accId =2 
+console.log("accId:"+ accId);
+
+accEmail = "rishi17@gmail.com"
+accpwd = "1212121"
+acccity="Vijaywada"
+
+console.table([accId,accEmail,accpwd,acccity,accstate])
